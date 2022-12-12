acfce99ffbb02371ee86ddf0cfa5406c55adb11b
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2022-12-13 02:24:40 +0700

    done

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Franciscus Valentinus Ongkosianbhadra on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
